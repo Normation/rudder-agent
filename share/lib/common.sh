8666e24d7f9de113ee1691aa4d8136444ed33568
Alexis Mousset <alexis.mousset@normation.com>
2016-01-15 10:25:23 +0100

    Fixes #7773: Backport the new rudder agent output to 3.1

---
+++ share/lib/common.sh
@@ -0,0 +1,33 @@
+# Reset colors
+clear_colors() {
+    COLOR=""
+    GREEN=""
+    DARKGREEN=""
+    RED=""
+    BLUE=""
+    PINK=""
+    WHITE=""
+    WHITELIGHT=""
+    MAGENTA=""
+    YELLOW=""
+    CYAN=""
+    NORMAL=""
+}
+
+# Colors configuration (enable colors only if stdout is a terminal)
+if [ -t 1 ]; then
+    COLOR="-Calways"
+    GREEN="\\033[1;32m"
+    DARKGREEN="\\033[0;32m"
+    RED="\\033[1;31m"
+    BLUE="\\033[1;34m"
+    TPINK="\\033[1;35m"
+    WHITE="\\033[0;02m"
+    WHITELIGHT="\\033[1;08m"
+    MAGENTA="\\033[1;35m"
+    YELLOW="\\033[1;33m"
+    CYAN="\\033[1;36m"
+    NORMAL="\\033[0;39m"
+else
+    clear_colors
+fi
