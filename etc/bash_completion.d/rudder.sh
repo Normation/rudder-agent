c3787d3be8f9fb48eb296f7e852a2b1f8dbba73e
Felix Dallidet <felix.dallidet@normation.com>
2019-08-19 17:14:26 +0200

    Fixes #15506: Change rudder agent bash autocomplete file to add rudder-pkg completion

--- etc/bash_completion.d/rudder.sh
+++ etc/bash_completion.d/rudder.sh
@@ -1,8 +1,24 @@
+_global() {
+  if [ $COMP_CWORD -ge 1 ]; then
+    case ${COMP_WORDS[1]} in
+      package)
+        if declare -f _rudderpkg > /dev/null; then
+          _rudderpkg
+        else
+          _rudder
+        fi
+      ;;
+      *)
+        _rudder
+      ;;
+    esac
+  fi
+}
 _rudder() {
   local cur="$2"
-  local prev="$3"  
+  local prev="$3"
   local obj cmd base opts
-  
+
   base="/opt/rudder/share/commands"
   # base objects: agent server remote ...
   obj=$(cd ${base} && ls -1 | cut -f 1 -d "-" | sort -u)
@@ -20,4 +36,4 @@ _rudder() {
     opts=$(grep '# @man \*-.\*:' "${base}/${COMP_WORDS[1]}-${COMP_WORDS[2]}" | sed -e 's/# @man \*\([^*]*\)\*.*/\1/')
     COMPREPLY=( $(compgen -W "help ${opts}" -- "${cur}") )
   fi
-} && complete -F _rudder rudder
+} && complete -F _global rudder
