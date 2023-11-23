3445cbd16c1e90aad6bd651efe5136df91325dc4
Benoît Peccatte <benoit.peccatte@normation.com>
2023-11-23 11:41:29 +0100

    Fixes #23598: Add next run time in rudder agent info

--- share/lib/get-splay.pl
+++ share/lib/get-splay.pl
@@ -33,10 +33,16 @@ my $str = <$file>;
 chomp $str;
 # cf-execd startup time
 my $startup_time = <$file>;
+chomp $startup_time;
 # configured splay
 my $splay = <$file>;
+chomp $splay;
 # agent run period in minute
 my $run_period = <$file>;
+chomp $run_period;
+# inventory job splay hash in hex
+my $inventory_splay_hash = <$file>;
+chomp $inventory_splay_hash;
 
 # splay as calculated by cf-execd
 my $hash = hash($str);
@@ -49,10 +55,16 @@ my $last_wake_up = $time - ($time % $run_period) + ($startup_time % 60);
 my $run_time = $last_wake_up + $splay_s;
 if ($run_time < $time) {
   # the agent has already run, add one period
-  $run_time += $period
+  $run_time += $run_period
 }
 
 # we could subtract $time directly in the formulas above but it would be harder to understand
 my $wait = $run_time - $time;
-print("$wait\n");
+
+# inventory run time
+my $inventory_id = hex($inventory_splay_hash) * (21600 / $run_period) / 268435455;
+my $inventory_startup = int($inventory_id) * $run_period;
+my $inventory_runtime = $inventory_startup + ($startup_time % 60) + $splay_s;
+
+print("$wait $inventory_runtime\n");
 
